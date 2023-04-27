11c18151a4931e0b9a696055c35f0c9b24391823
m-heim <MaximilianHeim@protonmail.com>
2023-04-27 21:26:57 +0200

    add rtf files

--- ret2fn/rtf.py
+++ ret2fn/rtf.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def get_buffer_len(file_name: str) -> int:
-    for i in range(1, 2048, 4):
+    for i in range(1, 2048):
         if os.system(f'{file_name} {"A" * i}'):
             return i
     raise RuntimeError('Couldnt determine buffer length')
--- ret2fn/vuln.c
+++ ret2fn/vuln.c
@@ -8,10 +8,10 @@ int vuln() {
 
 int main(int argc, char **argv) {
     if (argc <= 1) {
-        printf("Please hack me");
+        printf("Please hack me\n");
         return 1;
     }
-    char buf[8];
+    char buf[8] = {};
     strcpy(buf, argv[1]);
     return 0;
 }
